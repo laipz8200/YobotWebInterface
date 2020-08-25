8d1b3f927d064cc29c744001cb0e05b168ba5fd4
corvo007 <67192165+corvo007@users.noreply.github.com>
2020-08-25 16:30:17 +0800

    Update User Guide

--- WebUIOneKeyScript.ps1
+++ WebUIOneKeyScript.ps1
@@ -1,83 +1,97 @@
-﻿$ErrorActionPreference = "SilentlyContinue"
-$loop = $true
+﻿$ErrorActionPreference = "inquire"
 
-$choice = Read-Host "
-注意：执行操作前请确认本脚本位于yobot根目录下！
+Write-Output "
+注意：执行操作前请确认本脚本与内层yobot目录(插件版)/yobot目录(源码版)位于同级目录！
+
 本脚本当前执行位置：$PSScriptRoot
 ===============命令菜单==================
 1.全新安装
 2.卸载并恢复
 3.更新
 警告：若之前没有通过本脚本安装，请勿执行卸载命令！
-========================================
-请选择要执行的操作(请输入数字序号):
-"
+========================================"
+
+$choice = Read-Host "请选择要执行的操作(请输入数字序号)"
 
 
 switch ($choice) 
 {
     1 
     { 
-        $loop = $false
+        if (Test-Path .\YobotWebInterface)
+        {
+            Write-Output "已经有本项目文件夹了，建议去更新"
+            break
+        }
         git clone https://github.com/laipz8200/YobotWebInterface.git
         if (!$?) 
         {
             Write-Output "你没装git！/ 你网络不好！"
+            break
         }
-        New-Item .\yobot\src\client\pubilc\backup -ItemType Directory
-        Copy-Item .\yobot\src\client\pubilc\static .\yobot\src\client\pubilc\backup -Recurse -Force
-        Copy-Item .\yobot\src\client\pubilc\template .\yobot\src\client\pubilc\backup -Recurse -Force
-        Remove-Item .\yobot\src\client\pubilc\static -Force
-        Remove-Item .\yobot\src\client\pubilc\template -Force
-        Copy-Item  .\YobotWebInterface\static .\yobot\src\client\pubilc
-        Copy-Item  .\YobotWebInterface\template .\yobot\src\client\pubilc
+        New-Item .\yobot\src\client\public\backup -ItemType Directory
+        Copy-Item .\yobot\src\client\public\static .\yobot\src\client\public\backup -Recurse -Force
+        Copy-Item .\yobot\src\client\public\template .\yobot\src\client\public\backup -Recurse -Force
+        Remove-Item .\yobot\src\client\public\static -Force -Recurse
+        Remove-Item .\yobot\src\client\public\template -Force -Recurse
+        Copy-Item  .\YobotWebInterface\static .\yobot\src\client\public
+        Copy-Item  .\YobotWebInterface\template .\yobot\src\client\public
         Write-Output "安装完成！"
-     }
+        exit
+    }
     2 
     {
-        $loop = $false
-        Remove-Item .\yobot\src\client\pubilc\static -Force
-        Remove-Item .\yobot\src\client\pubilc\template -Force
-        Copy-Item  .\yobot\src\client\pubilc\backup\static .\yobot\src\client\pubilc
-        Copy-Item  .\yobot\src\client\pubilc\backup\template .\yobot\src\client\pubilc
-        Remove-Item .\yobot\src\client\pubilc\backup -Recurse -Force
+        Remove-Item .\yobot\src\client\public\static -Force -Recurse
+        Remove-Item .\yobot\src\client\public\template -Force -Recurse
+        Copy-Item  .\yobot\src\client\public\backup\static .\yobot\src\client\public
+        Copy-Item  .\yobot\src\client\public\backup\template .\yobot\src\client\public
+        Remove-Item .\yobot\src\client\public\backup -Recurse -Force
         Write-Output "卸载完成！"
+        exit
     } 
     3 
-    {
-        Test-Path .\YobotWebInterface
+    { 
+        if(Test-Path .\YobotWebInterface)
         {
             Set-Location .\YobotWebInterface
             git pull
             if (!$?) 
             {
                 Write-Output "你没装git！/ 你网络不好！"
+                break
             }
             Set-Location ..\
-            Test-Path .\yobot\src\client\pubilc\backup
+            if(Test-Path .\yobot\src\client\public\backup)
             {
-                Remove-Item .\yobot\src\client\pubilc\backup -Force -Recurse
-                New-Item .\yobot\src\client\pubilc\backup -ItemType Directory
-                Copy-Item .\yobot\src\client\pubilc\static .\yobot\src\client\pubilc\backup -Recurse -Force
-                Copy-Item .\yobot\src\client\pubilc\template .\yobot\src\client\pubilc\backup -Recurse -Force
+                Remove-Item .\yobot\src\client\public\backup -Force -Recurse
+                New-Item .\yobot\src\client\public\backup -ItemType Directory
+                Copy-Item .\yobot\src\client\public\static .\yobot\src\client\public\backup -Recurse -Force
+                Copy-Item .\yobot\src\client\public\template .\yobot\src\client\public\backup -Recurse -Force
             }
             else {
-                New-Item .\yobot\src\client\pubilc\backup -ItemType Directory
-                Copy-Item .\yobot\src\client\pubilc\static .\yobot\src\client\pubilc\backup -Recurse -Force
-                Copy-Item .\yobot\src\client\pubilc\template .\yobot\src\client\pubilc\backup -Recurse -Force
+                New-Item .\yobot\src\client\public\backup -ItemType Directory
+                Copy-Item .\yobot\src\client\public\static .\yobot\src\client\public\backup -Recurse -Force
+                Copy-Item .\yobot\src\client\public\template .\yobot\src\client\public\backup -Recurse -Force
             }
-            Remove-Item .\yobot\src\client\pubilc\static -Force
-            Remove-Item .\yobot\src\client\pubilc\template -Force
-            Copy-Item  .\YobotWebInterface\static .\yobot\src\client\pubilc
-            Copy-Item  .\YobotWebInterface\template .\yobot\src\client\pubilc
+            Remove-Item .\yobot\src\client\public\static -Force -Recurse
+            Remove-Item .\yobot\src\client\public\template -Force -Recurse
+            Copy-Item  .\YobotWebInterface\static .\yobot\src\client\public
+            Copy-Item  .\YobotWebInterface\template .\yobot\src\client\public
+            Write-Output "更新完成！"
+            exit
             
         }
-        else {
-            Write-Output "你原来的文件都没有你更新你ma呢？"
+        else 
+        {
+        Write-Output "没有项目文件夹，建议选择安装"
+        break
         }
     }
     Default 
     {
-        $loop = $true
+        Write-Output "你什么都没选哦~"
+        break
     }
 }
+
+
